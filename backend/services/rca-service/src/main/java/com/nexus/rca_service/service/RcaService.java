304e624813014bf7e77a5ef528d96a0bedd9033b
Sunny Solanki <sunnysolanki74860@gmail.com>
2026-09-12 23:15:22 +0530

    Revert "feat: integrate MCP client in rca-service"
    This reverts commit 804d7b4d00aca1b25d6b8edd8e063f59a6dd3a65.

--- backend/services/rca-service/src/main/java/com/nexus/rca_service/service/RcaService.java
+++ backend/services/rca-service/src/main/java/com/nexus/rca_service/service/RcaService.java
@@ -14,7 +14,6 @@ import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpande
 import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
 import org.springframework.ai.rag.retrieval.join.ConcatenationDocumentJoiner;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
-import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -41,7 +40,6 @@ public class RcaService {
     private final ObjectMapper objectMapper;
     private final VectorStore vectorStore;
     private final KafkaTemplate<Object, Object> kafkaTemplate;
-    private final ToolCallbackProvider toolCallbackProvider;
 
     @Value("classpath:/prompts/system.st")
     private Resource systemMessage;
@@ -110,11 +108,8 @@ public class RcaService {
                 .build();
         */
 
-
-
         RcaResponse rawResponse = chatClient
                 .prompt()
-                .tools(toolCallbackProvider)
                 .advisors(new SimpleLoggerAdvisor())
                 // .advisors(retrievalAugmentationAdvisor)
                 .system(systemMessage)
